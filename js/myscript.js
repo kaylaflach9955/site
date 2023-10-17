1d3b25494d8a45595a2c4b207d9742178569d57e
kaylaflach9955 <142498905+kaylaflach9955@users.noreply.github.com>
2023-10-17 09:50:44 -0300

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,8 @@
 function mudarFrase() {
-    var elemento = document.getElementById("frase");
-    fraseAtual.innerHTML= "Qualquer frase";
-    fraseAtual.style.color="red";
-    fraseAtual.style.fontSize="25px";
-    fraseAtual.style.backgroundColor="Yellow"
-    document.body.style.backgroundColor= "blue"
-    Element.innerHTML ="img src=\'img/a.jpeg\' />"
+    var fraseAtual = document.getElementById("frase");
+    fraseAtual.innerHTML = "Qualquer frase";
+    fraseAtual.style.color = "red";
+    fraseAtual.style.fontSize = "25px";
+    fraseAtual.style.backgroundColor = "Yellow";
+    document.body.style.backgroundColor = "blue";
 }
