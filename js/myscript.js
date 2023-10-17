76ff96507864463200837b1c081770c4617f7c19
kaylaflach9955 <142498905+kaylaflach9955@users.noreply.github.com>
2023-10-17 11:28:25 +0000

    a

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function mudarFrase() {
+    var elemento = document.getElementById("frase");
+    fraseAtual.innerHTML= "Qualquer frase";
+    fraseAtual.style.color="red";
+    fraseAtual.style.fontSize="25px";
+    fraseAtual.style.backgroundColor="Yellow"
+    document.body.style.backgroundColor= "blue"
+    Element.innerHTML ="img src=\'img/a.jpeg\' />"
+}
